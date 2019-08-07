b0ce3338949192fbc4896a29ad935a4d35e6602c
Anirban sengupta <sengupta.ani.80@gmail.com>
2019-08-07 12:17:03 +0530

    Main Function

---
+++ Springdemo/src/spring/demo/Drawing.java
@@ -0,0 +1,19 @@
+package spring.demo;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+public class Drawing {
+
+	public static void main(String[] args) {
+   Resource resource=new ClassPathResource("SpringXML.xml");
+	BeanFactory fac=new XmlBeanFactory(resource);
+	fac.getBean("triangle");
+	Triangle triangle = (Triangle)fac.getBean("triangle");
+	triangle.draw();
+	}
+
+}
