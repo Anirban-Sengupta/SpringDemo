5d9b8882bd7073b23265857f9b27373a29d68577
Anirban sengupta <sengupta.ani.80@gmail.com>
2019-08-07 12:40:54 +0530

    removed unused imports

--- Springdemo/src/spring/demo/Drawing.java
+++ Springdemo/src/spring/demo/Drawing.java
@@ -3,13 +3,12 @@ package spring.demo;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 
 public class Drawing {
 
 	public static void main(String[] args) {
-   Resource resource=new ClassPathResource("SpringXML.xml");
+    Resource resource=new ClassPathResource("SpringXML.xml");
 	BeanFactory fac=new XmlBeanFactory(resource);
 	fac.getBean("triangle");
 	Triangle triangle = (Triangle)fac.getBean("triangle");
